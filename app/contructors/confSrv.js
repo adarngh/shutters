958a477f50f5d86571bcba997d775145de8db2ac
Adar Noy <adar.noy@gmail.com>
2018-07-30 20:33:12 +0300

    טופס הגדרה

---
+++ app/contructors/confSrv.js
@@ -0,0 +1,18 @@
+shuttersApp.factory("confSrv", function ($http, $q) {
+    function Proposal(mName, mLength, mActors, mDirector, posterUrl, imdbUrl, tmdbId) {
+        this.pNumber = pNumber;
+        this.pConsNo = pConsNo;
+        this.pWidth = pWidth;
+        this.pHeight = pHeight;
+        this.pProfileType = pProfileType;
+        this.pNoOfWings = pNoOfWings;
+        this.pNoOfFields =[];
+        this.pColor
+        this.pSliding
+    }
+
+
+    return{
+        getAllPosts: getAllPosts
+    }
+});
