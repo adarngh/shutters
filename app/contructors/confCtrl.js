7a5ffa79d1f2c242ad24730318298731a8b4a682
Adar Noy <adar.noy@gmail.com>
2018-08-01 18:15:12 +0300

    order list page

--- app/contructors/confCtrl.js
+++ app/contructors/confCtrl.js
@@ -1,8 +1,13 @@
 shuttersApp.controller("confCtrl", function ($scope, $routeParams,ordersSrv) {
-    ordersSrv.getOrders();
+    // ordersSrv.getOrders();
+    $scope.orderList=ordersSrv.orderList;
+    $scope.itemList=[];
+    $scope.currentOrder={};
     $scope.numberOfFields=1;    
     $scope.leverType=new Array(5);
-    $scope.fieldArray=[{ "fieldType": $scope.leverType[0] }];
+    $scope.fieldArray=[{ "fieldType": $scope.leverType[0] }]; 
+    
+    
     $scope.changeFieldArray = function () {
         $scope.fieldArray.splice(0);
         for (var i1 = 0; i1 < $scope.numberOfFields; i1++) {
@@ -12,6 +17,9 @@ shuttersApp.controller("confCtrl", function ($scope, $routeParams,ordersSrv) {
 
 
 
+    }
+    $scope.getOrderDetails =function(order) {
+        $scope.currentOrder=order;
     }
 
 
--- app/contructors/ordersSrv.js
+++ app/contructors/ordersSrv.js
@@ -2,8 +2,8 @@ shuttersApp.factory("ordersSrv", function ($http, $q) {
     var temp
     var orderList = [];
     var ordersUrl = "./private/jsonFiles/orders.json"
-
-    function Order(oNumber, oOrderOwner, oWidth, oHeight, oProfileType, oNoOfWings, oFields, oColor, oIsSliding, oSpruts, oSprutsHeigt, oBattery,oItemList) {
+    getOrders()
+    function Order(oNumber, oOrderOwner, oWidth, oHeight, oProfileType, oNoOfWings, oFields, oColor, oIsSliding, oSpruts, oSprutsHeigt, oBattery,oItemList,oDateTime,oRemarks) {
         this.oNumber = oNumber;
         this.oOrderOwner = oOrderOwner;
         this.oWidth = oWidth;
@@ -17,15 +17,19 @@ shuttersApp.factory("ordersSrv", function ($http, $q) {
         this.oSprutsHeigt = oSprutsHeigt;
         this.oBattery = oBattery;
         this.oItemList = oItemList;
+        this.oDateTime=oDateTime;
+        this.oDateTime=oDateTime;
+        this.oRemarks=oRemarks;
     }
 
     function getOrders() {
         $http.get(ordersUrl).then(function (response) {
+            orderList.splice(0);
             var result = response.data;
             for (var i1 = 0; i1 < result.length; i1++) {
                 orderList.push(new Order(result[i1].oNumber, result[i1].oOrderOwner, result[i1].oWidth, result[i1].oHeight, result[i1].oProfileType,
                     result[i1].oNoOfWings, result[i1].oFields, result[i1].oColor, result[i1].oIsSliding,result[i1].oSpruts, result[i1].oSprutsHeigt,
-                    result[i1].oBattery, result[i1].oItemList));
+                    result[i1].oBattery, result[i1].oItemList,result[i1].oDateTime,result[i1].oRemarks));
             }
 
             var stop = null;
@@ -38,7 +42,8 @@ shuttersApp.factory("ordersSrv", function ($http, $q) {
 
     return {
         Order: Order,
-        getOrders: getOrders
+        getOrders: getOrders,
+        orderList:orderList
     }
 
 
