0b05c0b70a5c78a60b42c4924a300d8f951de29d
Adar Noy <adar.noy@gmail.com>
2018-08-06 23:51:15 +0300

    print order

--- app/contructors/newOrderCtrl.js
+++ app/contructors/newOrderCtrl.js
@@ -24,23 +24,26 @@ shuttersApp.controller("newOrderCtrl", function ($scope, $timeout, $location, or
     }
     printDiv = function (divName) {
         var printContents = document.getElementById(divName).innerHTML;
-        var popupWin = window.open('', '_blank', 'width=300,height=300');
+        var popupWin = window.open('', '_blank', 'resizable,menubar=1,width=800,height=900');
         popupWin.document.open();
-        popupWin.document.write('<!DOCTYPE html><html ng-app="shuttersApp" dir="rtl"><head><meta charset="utf-8" /><meta http-equiv="X-UA-Compatible" content="IE=edge">'
-        +'<title>Shutters</title><meta name="viewport" content="width=device-width, initial-scale=1.0">'
-        +'<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">'
-        +'<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">'
-        +'<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt"crossorigin="anonymous">'
-        +'<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.slim.min.js">'
-        +'</script><script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.13.0/umd/popper.min.js">'
-        +'</script><script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js">'
-        +'</script><script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script><script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular-route.js">'
-        +'</script><script src="./app/shuttersApp.js"></script><script src="./app/distributors/distCtrl.js"></script>'
-        +'<script src="./app/contructors/userSrv.js"></script><script src="./app/contructors/userCtrl.js"></script><script src="./app/contructors/orderCtrl.js"></script>'
-        +'<script src="./app/contructors/newOrderCtrl.js"></script><script src="./app/contructors/orderSrv.js"></script><script src="./app/home/homeCtrl.js"></script>'
-        +'<link rel="stylesheet" type="text/css" media="screen" href="./app/style.css" /></head><body ng-controller="newOrderCtrl">'         
-        + printContents + '</body></html>');
+        popupWin.document.write('<!DOCTYPE html><html ng-app="shuttersApp" ><head><meta charset="utf-8" /><meta http-equiv="X-UA-Compatible" content="IE=edge">'
+            + '<title>Shutters</title><meta name="viewport" content="width=device-width, initial-scale=1.0">'
+            + '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">'
+            + '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">'
+            + '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt"crossorigin="anonymous">'
+            + '<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.slim.min.js">'
+            + '</script><script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.13.0/umd/popper.min.js">'
+            + '</script><script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js">'
+            + '</script><script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script><script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular-route.js">'
+            + '</script><script src="./app/shuttersApp.js"></script><script src="./app/distributors/distCtrl.js"></script>'
+            + '<script src="./app/contructors/userSrv.js"></script><script src="./app/contructors/userCtrl.js"></script><script src="./app/contructors/orderCtrl.js"></script>'
+            + '<script src="./app/contructors/newOrderCtrl.js"></script><script src="./app/contructors/orderSrv.js"></script><script src="./app/home/homeCtrl.js"></script>'
+            + '<link rel="stylesheet" type="text/css" media="screen" href="./app/style.css" /></head><body onload="window.print()" ng-controller="newOrderCtrl">'
+            + printContents + '</body></html>');
         popupWin.document.close();
+        // popupWin.document.focus();
+        // popupWin.document.print();
+        // popupWin.document.close();
     }
 
 
@@ -236,14 +239,12 @@ shuttersApp.controller("newOrderCtrl", function ($scope, $timeout, $location, or
         var newOrder = new orderSrv.Order(orderSrv.getNewOrderNum(), activeUser.cNumber, $scope.pWidth, $scope.pHeight, $scope.pProfileType, $scope.pNoOfWings, tempField, $scope.pColor, $scope.pIsSliding, $scope.pSpruts, $scope.pSprutsHeigt, $scope.pBattery, itemList, dateText, $scope.pRemarks);
         $scope.orderList.push(newOrder);
         $scope.newOrder = newOrder;
-        $timeout(function(){printDiv("ordetoprint")},3000);
-        // $timeout(printDiv
-
-
         
-        // $location.path("/orderlist");
+        $location.path("/orderlist");
+        // $timeout(function () { $scope.orderList.push(newOrder) });
+        // $timeout(function () { printDiv("ordetoprint") });
+        // $timeout(function () { $location.path("/orderlist") });
 
-        var stop = null;
     }
 });
 
