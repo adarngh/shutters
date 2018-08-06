fbdbbba9cb29b24ddc230f0c2fb9cf9d60b0085f
Adar Noy <adar.noy@gmail.com>
2018-08-06 10:04:40 +0300

    login fix +login page

--- app/distributors/distCtrl.js
+++ app/distributors/distCtrl.js
@@ -1,10 +1,14 @@
 shuttersApp.controller("distCtrl", function ($scope, $sce, userSrv, $routeParams) {
     $scope.userList = userSrv.userList;
     $scope.address = $sce.trustAsResourceUrl("https://www.google.com/maps/embed/v1/place?key=AIzaSyDj2_x8-Phi_rdxi2_UBOgDlOydfPEf2BU&language=he&zoom=10&q=tel aviv");
+    
+
+    $scope.showMap= function(xUser){
+
         var userAdress = (xUser.cAdress + "+" + xUser.cCity).replace(/\s+/g, '+');
         $scope.address = $sce.trustAsResourceUrl("https://www.google.com/maps/embed/v1/place?key=AIzaSyDj2_x8-Phi_rdxi2_UBOgDlOydfPEf2BU&language=he&zoom=13&q=" + userAdress);
+
     }
-    // <!--google map API:AIzaSyDj2_x8-Phi_rdxi2_UBOgDlOydfPEf2BU-->
 
 
 });
